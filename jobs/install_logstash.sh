3a8d870eea88ca0b21f5efca9fea43769731fed1
Matt Eary <matt@earys.com>
2015-08-07 17:34:56 -0500

    added logic for logstash.conf

--- jobs/install_logstash.sh
+++ jobs/install_logstash.sh
@@ -6,3 +6,6 @@ wget https://download.elastic.co/logstash/logstash/packages/centos/logstash-1.5.
 rpm -ivh logstash-1.5.3-1.noarch.rpm
 
 setfacl -m u:logstash:r /var/log/{messages,secure,yum.log}
+
+cp logstash_agents/logstash.conf /etc/logstash/conf.d/ && chmod 644 /etc/logstash/conf.d/*
+
